7c08a7dbd89fab95082f0335fa377580b897ecc6
Mateusedu14 <mateus.elazevedo@senacsp.edu.br>
2025-01-23 21:23:10 -0300

    Feat: Usando querySelector para Capturar Valores no HTML

---
+++ JS/objeto.js
@@ -0,0 +1,44 @@
+let botao = document.querySelector("#btnMostrar");
+botao.addEventListener("click", mostrarValores);
+
+
+
+function mostrarValores() {
+    let pessoa = {};
+
+    pessoa.nome = document.querySelector('#txtNome').value; 
+    pessoa.altura = parseFloat(document.querySelector('#txtAltura').value); 
+    pessoa.idade = parseInt(document.querySelector('#txtIdade').value); 
+    pessoa.dataNascimento = document.querySelector('#txtDataNascimento').value;
+
+    let mensagem = `
+    <br><br>
+    O nome digitado é <strong> ${pessoa.nome} </strong> <br>
+    A altura informada é <strong>${pessoa.altura.toFixed(2)} </strong> <br>
+    A idade informada é <strong> ${pessoa.idade} </strong> <br>
+    A data de nascimento informada é <strong> ${pessoa.dataNascimento} </strong> `;
+
+
+let div = document.querySelector("#resultado");
+div.innerHTML = mensagem;
+
+
+
+}
+
+// let pessoa = {};
+// pessoa.nome = prompt("Informe Nome");
+// pessoa.altura = parseFloat(prompt("Informe Altura:"));
+// pessoa.idade = parseInt(prompt("Informe Idade:"));
+// pessoa.dataNascimento = prompt(
+//     "Informe Data Nascimento (dd/MM/yyyy)");
+
+// console.log(pessoa);
+// alert(
+//     `O nome digitado é ${pessoa.nome}
+//     A altura informada é ${pessoa.altura.toFixed(2)}
+//     A idade informada é ${pessoa.idade}
+//     A data de nascimento informada é ${pessoa.dataNascimento}
+//     `
+// );
+
